40429093128e13cebaa29b5a21e8c653244a9a5b
Nio kasgami <devil_nicolas@hotmail.com>
2016-08-05 21:07:31 -0700

    Project Configurations

---
+++ dream_engine/dream_engine/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/**
+ *  Dream Engine GulFile
+ *  @description This file serve for the merrely purpose of compile and concatenate Typescript files and shouldn't be edited other than by the original author
+ *  @version 1.00
+ *  @author Nio Kasgami
+ */
+
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+
+function TsConfig(file) {
+    return ts({
+        allowJs: true,
+        module: "commonjs",
+        noEmitOnError: false,
+        noImplicitAny: true,
+        removeComments: true,
+        sourceMap: false,
+        target: "es5",
+        outFile: `Dream_${file}.js`,
+        typescript: require('typescript') // use local installation of typescript (who are 2.00)
+    });
+}
+
+gulp.task('dist', ['Core']);
